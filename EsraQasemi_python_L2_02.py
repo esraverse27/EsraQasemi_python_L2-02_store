633f9a13b9518de7f12be5804bb3d16f52732dfa
esraverse <qasemiesra411@gmail.com>
2026-09-15 18:29:11 +0330

    I just updated the store

---
+++ EsraQasemi_python_L2_02.py
@@ -0,0 +1,154 @@
+class User:
+    def __init__(self, role:str , userName:str , password:str ):
+        self.role = role
+        self.userName = userName
+        self.password = password
+        self.is_logged_in = False
+        self.cart_items = []
+
+
+    users_list = []
+
+    def login(self, userName, password):
+    #checking if input information is true, and logging in
+        try:
+            for user in self.users_list:
+                if user.userName==userName and user.password==password:
+                    user.is_logged_in = True
+                    break
+            else:
+                raise ValueError
+        except:
+            print("user name or password is incorrect")
+            user.is_logged_in = False
+
+
+    def sign_in(self, role, userName, password):
+        try:
+            for user in self.users_list:
+                if user.userName == userName and user.password == password:
+                    raise ValueError("already signed in")
+            else:
+                user = User(role, userName, password)
+                self.users_list.append(user)
+        except ValueError:
+            print("already signed in")
+
+    def user_logout(self):
+        for user in self.users_list:
+            if user.is_logged_in:
+                for item in user.cart_items:
+                    for product in store.list_of_products:
+                        if item == product:
+                            product.stock += item.quantity
+                user.cart_items.clear()  
+
+class product:
+#information of each product will be defined here
+    def __init__(self, name:str , price:float ,stock:int):
+        self.name = name
+        self.price = price
+        self.stock = stock
+
+    def __str__(self):
+        return f"Product Name:{self.name} _ Price:{self.price}$ _ Stock:{self.stock}"
+
+class store:
+    def __init__(self):
+        self.list_of_products = []
+        #a list to store product objects in it
+
+    def add_product(self, product:product):
+        try:
+            if product in self.list_of_products:
+                raise ValueError
+            else:
+                self.list_of_products.append(product)
+        except:
+            print("the product already exists")
+
+
+class cartItem:
+#cart Items contain an object of product and quantity. then we save theme in a list of the next class
+    def __init__(self, product:product , quantity ):
+        self.product = product
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"name:{self.product.name} _ price:{self.product.price} _ quantity:{self.quantity} _ total price:{int(self.product.price)*int(self.quantity)} "
+
+class cart:
+#to define operations on cart
+
+    def add_to_cart(self, product_name:str, quantity:int, store):  
+        for user in User.users_list:
+            if user.is_logged_in:  
+                for the_product in store.list_of_products:
+                    if the_product.name == product_name:
+                    #it means the wanted object found in store list
+
+                        #when an object of class cartItem, exists in cart
+                        try:
+                            for i in user.cart_items:
+                                if i.product == the_product:
+                                    if i.product.stock < quantity :
+                                        raise ValueError("Sorry, the product inventory is not enough")
+                                    else:
+                                        i.quantity += quantity
+                                        i.product.stock -= quantity
+                                        print("{product_name} was added to cart successfuly")
+                                        return
+                        except:
+                            print("Sorry, the product inventory is not enough")
+                
+                #when the product doesn't exist in cart and we have to creat an object for it      
+                else: 
+                    item = cartItem(the_product, quantity)
+                    user.cart_items.append(item)
+                    the_product.stock -= quantity
+                    return 
+        else:
+            print("Sorry, we don't have this product")
+
+            
+    def remove_from_cart(self, product_name:str, store):
+        for user in User.users_list:
+            if user.is_logged_in: 
+                for item in user.cart_items:
+                    if item.product.name == product_name:
+                        user.cart_items.remove(item)
+                    #to find the product in store list and return its quantity
+                        for product in store.list_of_products:
+                            if product == item.product:
+                                product.stock += item.quantity
+                        return
+        else:
+            print("there isn't this item in your cart")
+
+        
+    def view_items(self):
+        for user in User.users_list:
+            if user.is_logged_in:
+                if not user.cart_items:
+                    print("still empty..")
+                else:
+                    for index, item in enumerate(user.cart_items):
+                        print(f"{index+1}. ", item)
+                    print("")
+
+
+    def Checkout(self):
+        for user in User.users_list:
+            if user.is_logged_in: 
+                if user.cart_items:
+                    self.view_items()
+                    confirm = input("Are you sure you want to chechout? (y/n)")
+
+                    if confirm.lower() != "y" :
+                        print("checkout cancelled!")
+                        return
+
+                    else:
+                        user.cart_item.clear()
+                        print("Thanks for shopping:)")
+
--- General code.py
+++ General code.py
@@ -1,92 +1,161 @@
 import EsraQasemi_python_L2_02 as classes
 
+my_user = classes.User("","","")
+users_list = my_user.users_list
+
 my_store = classes.store()  #to recall this class 
-list_store = my_store.prod_list #renaming the list
+list_store = my_store.list_of_products #renaming the list
 
 cart = classes.cart() #to racall the class
-cart_items = cart.items #renaming the list
 
 while True :
+    try:
+        print("1.sign in")
+        print("2.log in")
+        print("3.End code")
+        choise = int(input("enter your choise: "))
+        print("")
+    except TypeError:
+        print("enter the number of your choise")
+        continue
 
-    role = input("Please select your role. Manager / Customer (or Exit to end the code): ")
-    print("")
-    #authentication
-    if role not in ["Manager","Customer","Exit"]:
-        raise ValueError("invalid input!")
-    elif role == "Exit":
-        break
+    match choise:
 
-    if role == "Manager" :
-        print("..manager menu..\n")
+        case 1:
+            role = ""
+            while role not in ['customer','customer','manager','Manager']:
+                role = input("Enter your role: (Manager/Customer) ")
+            user_name = input("Enter your UserName: ")
+            password = input("Enter your password: ")
 
-    while role == "Manager" :
-        print("to add the product..")
-        print("Enter 0 to change role")
-        product_name = input("Enter the name of product: ")
-        if product_name == "0":
-            break
-        product_price = input("Enter the price of product: ")
-        if product_price == "0":
-                    break
-        product_stock = int(input("Enter the stock quantity: "))
-        if product_stock == "0":
-                    break
-
-    #creating an object of class object and adding it to list store
-        new_prod = classes.product(product_name, product_price, product_stock)
-        my_store.add_product(new_prod)
-
-    #here the manager can see the list of store products
-        print("َShop products:")
-        print("added successfuly.\n")
-        for obj in list_store:
-            print(obj)
-        else:
-            print("")     
-
-
-    if role == "Customer":
-         print("..Customer Menu..")
-         print("welcome\n")
-         
-    while role == "Customer":
-
-    #showing information to user
-        print("Available products:")
-        for obj in list_store:
-            print(obj)
-        else:
+            my_user.sign_in(role,user_name,password)
             print("")
 
-    #getting input about what is the user going to do
-        print("what do you want to do?")
-        print("1. add item to cart")
-        print("2. remove item from cart")
-        print("3. view cart")
-        print("4. return to main menu \n")
-        task = int(input("Enter character of your choise: "))
+        case 2:
+            try:
+                user_name = input("Enter your UserName: ")
+                password = input("Enter your password: ")
+                my_user.login(user_name,password)
+            except ValueError: #the ValueError is raised manually in login function
+                continue
+            else:
+                pass
+            print("")
 
-    #adding product to cart
-        if task == 1:
-            item_name = input("enter the product name: ")
-            quantity = int(input("enter the quantity: "))
-            cart.add_to_cart(item_name, quantity, my_store)
-            print(f"{item_name} added successfuly.")
-
-    #removing an object from cart
-        if task == 2:
-            item_name = input("enter the product name: ")
-            cart.remove_from_cart(item_name, my_store)
-            print(f"{item_name} was removed from cart successfuly.")
-
-        if task == 3:
-            cart.view_items()
-
-    #to go back of customer's loop
-        if task == 4:
+        case 3:
             break
 
-        
+        case _:
+            print("invalid input!")
+            print("")
+            continue
+
+    for user in users_list:
+        while user.is_logged_in:
+            if user.role in ["Manager","manager"]:
+                try:
+                    print("Manager menu")
+                    print("1. add a product")
+                    print("2. see the store products")
+                    print("3. back to main menu")
+                    choise = int(input("Enter your choise: "))
+                    print("")
+                except TypeError:
+                    print("enter the number of your choise")
+                    continue
+
+                match choise:
+                    case 1:
+                    #giving input to creat a new product
+                        try:
+                            product_name = input("Enter the name of product: ")
+                            product_price = input("Enter the price of product: ")
+                            product_stock = int(input("Enter the stock quantity: "))
+                        except TypeError:
+                            print("enter an appropriate number for stock")
+
+                    #creating an object of class product and adding it to list store
+                        new_product = classes.product(product_name, product_price, product_stock)
+                        my_store.add_product(new_product)
+                        print("added successfully")
+                        print("")
+
+                    #here the manager can see the list of store products
+                        print("َShop products:")
+                        for index,object in enumerate(list_store):
+                            print(f"{index+1}. {object}")
+                        else:
+                            print("")  
+
+                    case 2:
+                    #to show list of products
+                        print("َShop products:")
+                        for index,object in enumerate(list_store):
+                            print(f"{index+1}. {object}")
+                        else:
+                            print("")  
+
+                    case 3:
+                    #it breaks the current loop and backs to main menu
+                        user.is_logged_in = False
+
+                    case _:
+                        print("invalid input!")
+
+
+            elif user.role in ["customer","Customer"]:
+                print("..Welcome to our shop..")
+                print("")
+                
+                print("Available products:")
+                for index,product in enumerate(list_store):
+                    print(f"{index+1}. {product}")
+                else:
+                    print("")
+
+                while True:
+                    try:
+                        print("what do you want to do?")
+                        print("1. add item to cart")
+                        print("2. remove item from cart")
+                        print("3. view cart")
+                        print("4. return to main menu")
+                        task = int(input("Enter the number of your choise: "))
+                        print("")
+                    except TypeError:
+                        print("enter a valid choise")
+
+
+                    match task:
+
+                    #adding product to cart
+                        case 1:
+                            try:
+                                item_name = input("enter the product name: ")
+                                quantity = int(input("enter the quantity: "))
+                                cart.add_to_cart(item_name, quantity, my_store)
+                                print(f"{item_name} added to cart successfuly.")
+                                print("")
+                            except TypeError:
+                                print("enter a valid number as quantity")
+
+                    #removing a product from cart
+                        case 2:
+                            item_name = input("enter the product name: ")
+                            cart.remove_from_cart(item_name, my_store)
+                            print(f"{item_name} was removed from cart successfuly.")
+                            print("")
+
+                        case 3:
+                            cart.view_items()
+
+                    #to go back to main menu
+                        case 4:
+                            user.user_logout()
+                            user.is_logged_in = False
+                            break
+
+    
 
 
 
